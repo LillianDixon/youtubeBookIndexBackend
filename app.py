a3195b735e79d82f7e0b1b7bd1c502724a0bb919
Lillian Dixon <dixon.lillian92@gmail.com>
2020-05-26 09:02:43 -0600

    youtube bookIndex backend

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nfguungcfacwou:56c7978349cfd5cf75b8ef020f57484a0afe5f8dbec4e50f58c0e6567de55f01@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d803vl9ihrme0c'
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
@@ -26,6 +26,48 @@ class Book(db.Model):
 @app.route("/")
 def home():
     return"<h1>Hi from Flask</h1>"
+
+@app.route('/book/input', methods=['POST'])
+def books_input():
+    if request.content_type == 'application/json':
+        post_data = request.get_json()
+        title = post_data.get('title')
+        author = post_data.get('author')
+        reg = Book(title, author)
+        db.session.add(reg)
+        db.session.commit()
+        return jsonify('Data Posted')
+    return jsonify('Something went terribly wrong')
+
+@app.route('/books', methods=["GET"])
+def return_books():
+    all_books = db.session.query(Book.id, Book.title, Book.author).all()
+    return jsonify(all_books)
+
+@app.route('/book/<id>', methods=['GET'])
+def return_single_book(id):
+    one_book=db.session.query(Book.id, Book.title, Book.author).filter(Book.id == id).first()
+    return jsonify(one_book)
+
+@app.route('/delete/<id>', methods=["DELETE"])
+def book_delete(id):
+    record = db.session.query(Book).get(id)
+    db.session.delete(record)
+    db.session.commit()
+    return jsonify('Deleted')
+
+@app.route('/update_book/<id>', methods=['PUT'])
+def book_update(id):
+    if request.content_type == 'application/json':
+        put_data = request.get_json()
+        title = put_data.get('title')
+        author = put_data.get('author')
+        record = db.session.query(Book).get(id)
+        record.title = title
+        record.author = author
+        db.session.commit()
+        return jsonify('update completed')
+    return jsonify("something went horribly wrong")
 
 if __name__ == "__main__":
     app.debug = True
